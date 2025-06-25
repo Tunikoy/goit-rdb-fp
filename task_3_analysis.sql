04dd062693c6ea54a864a6d739ed77e7a4879477
Alex Tunyk <93491720+Tunikoy@users.noreply.github.com>
2025-06-25 13:49:34 +0300

    task_3_analysis.sql

--- task_3_analysis.sql
+++ task_3_analysis.sql
@@ -5,9 +5,10 @@ SELECT
     MIN(d.number_rabies) AS min_rabies,
     MAX(d.number_rabies) AS max_rabies,
     SUM(d.number_rabies) AS total_rabies
-FROM pandemic.infectious_data d
+FROM pandemic.normalized_cases d
 JOIN pandemic.entities e ON d.entity_id = e.id
 WHERE d.number_rabies IS NOT NULL
 GROUP BY e.entity_name, e.code
 ORDER BY avg_rabies DESC
 LIMIT 10;
+
